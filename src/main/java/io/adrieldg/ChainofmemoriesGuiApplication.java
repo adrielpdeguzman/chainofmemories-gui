59262b61c7c4dced66bc7dfc0f5dbab379cc504c
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-05-31 16:33:56 +0800

    Initial commit

---
+++ src/main/java/io/adrieldg/ChainofmemoriesGuiApplication.java
@@ -0,0 +1,12 @@
+package io.adrieldg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainofmemoriesGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainofmemoriesGuiApplication.class, args);
+	}
+}
