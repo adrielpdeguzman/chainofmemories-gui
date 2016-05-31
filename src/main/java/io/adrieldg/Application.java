55940e6d890b997cbaf5538a2f52c31ee4bfc20e
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-05-31 17:24:39 +0800

    Started constructing main template. Included base webjars

--- src/main/java/io/adrieldg/Application.java
+++ src/main/java/io/adrieldg/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChainofmemoriesGuiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChainofmemoriesGuiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ src/main/java/io/adrieldg/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package io.adrieldg.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	String index(Model model) {
+		model.addAttribute("name", "Monalice");
+		return "index";
+	}
+}
