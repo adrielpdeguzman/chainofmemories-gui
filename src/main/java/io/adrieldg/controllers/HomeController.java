bc5b47dc4117931cbbac50e05d09866adcb6bc89
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-06-01 17:05:57 +0800

    Initial setup of views based on parent project (will be changed later on)

--- src/main/java/io/adrieldg/controllers/HomeController.java
+++ src/main/java/io/adrieldg/controllers/HomeController.java
@@ -13,4 +13,24 @@ public class HomeController {
   String index(Model model) {
     return "index";
   }
+
+  @RequestMapping(path = "/login", method = RequestMethod.GET)
+  String login() {
+    return "login";
+  }
+
+  @RequestMapping(path = "/login", method = RequestMethod.POST)
+  String doLogin() {
+    return "login";
+  }
+
+  @RequestMapping(path = "/logut", method = RequestMethod.GET)
+  String logout() {
+    return null;
+  }
+
+  @RequestMapping(path = "/changelogs", method = RequestMethod.GET)
+  String changelogs() {
+    return "changelogs";
+  }
 }
