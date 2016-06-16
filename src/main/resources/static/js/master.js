295013d69a95871f772818a38eb93c273905dcad
Adriel P. de Guzman <adeguzman@stratpoint.com>
2016-06-16 15:35:38 +0800

    Added JS function to fetch cookie
    - Will start ReactJS study now. Will resume with GUI afterwards

--- src/main/resources/static/js/master.js
+++ src/main/resources/static/js/master.js
@@ -1,3 +1,9 @@
+function getCookie(name) {
+  var re = new RegExp(name + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
+}
+
 $(function() {
   $(".alert").delay(4000).slideUp(200, function() {
     $(this).alert('close');
